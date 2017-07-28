716b1c7ca3010fa5c8a06e86ac49b4d7b119a13d
Mohamed <mohamedr201@gmail.com>
2017-07-28 17:10:21 -0400

    Intial commit

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {tokenNotExpired} from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
@@ -15,14 +16,16 @@ export class AuthService {
   registerUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('users/register',user,{headers: headers})
+    let ep = this.prepEndpoint('users/register');
+    return this.http.post(ep, user,{headers: headers})
       .map(res => res.json());
   }
 
   authenticateUser(user){
     let headers = new Headers();
     headers.append('Content-Type','application/json');
-    return this.http.post('users/authenticate', user,{headers: headers})
+    let ep = this.prepEndpoint('users/authenticate');
+    return this.http.post(ep, user,{headers: headers})
       .map(res => res.json());
   }
 
@@ -31,7 +34,8 @@ export class AuthService {
     this.loadToken();
     headers.append('Authorization', this.authToken);
     headers.append('Content-Type','application/json');
-    return this.http.get('users/profile',{headers: headers})
+    let ep = this.prepEndpoint('users/profile');
+    return this.http.get(ep,{headers: headers})
       .map(res => res.json());
   }
 
@@ -48,12 +52,20 @@ export class AuthService {
   }
 
   loggedIn(){
-    return localStorage.getItem('id_token') !==null;
+    return localStorage.getItem('id_token') !== null;
   }
 
-  logOut(){
+  logout(){
     this.authToken = null;
     this.user = null;
     localStorage.clear();
+  }
+
+  prepEndpoint(ep){
+    if(this.isDev){
+      return ep;
+    } else {
+      return 'http://localhost:8080/'+ep;
+    }
   }
 }

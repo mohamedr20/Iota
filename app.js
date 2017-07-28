5e4c5bd0b137b05692418c0d02cf1b39e71998d6
Mohamed <mohamedr201@gmail.com>
2017-07-28 19:10:05 -0400

    Changed index.html directory

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -49,7 +49,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/src/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start Server
